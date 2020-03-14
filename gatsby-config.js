e7772329fcc5cef2788ff77fefd050b1a4cfa2ba
marcoazzurrini <marco.azzurrini.art@gmail.com>
2020-03-14 17:55:14 +0100

    add service worker and gatsby-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,23 @@
 var proxy = require("http-proxy-middleware")
 
 module.exports = {
-  plugins: [`gatsby-plugin-emotion`, `gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-emotion`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `AA Frost UK`,
+        short_name: `AA Frost`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#127EB1`,
+        display: `standalone`,
+        icon: `src/img/icon.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
   developMiddleware: app => {
     app.use(
       "/.netlify/functions/",
